2e79360df064203efa02d0ac9f4d529c85b2abc2
leonux <leo.melgar33@gmail.com>
2015-04-02 16:46:28 -0300

    modificado el metodo list para que filtre por campos(todabia no funciona)

--- app/controllers/personas.server.controller.js
+++ app/controllers/personas.server.controller.js
@@ -72,16 +72,54 @@ exports.delete = function(req, res) {
 /**
  * List of Personas
  */
+// exports.list = function(req, res) { 
+// 	Persona.find().sort('apellido').populate('user', 'displayName').exec(function(err, personas) {
+// 		if (err) {
+// 			return res.status(400).send({
+// 				message: errorHandler.getErrorMessage(err)
+// 			});
+// 		} else {
+// 			res.jsonp(personas);
+// 		}
+// 	});
+// };
+/**
+ * List of Personas
+ */
 exports.list = function(req, res) { 
-	Persona.find().sort('-created').populate('user', 'displayName').exec(function(err, personas) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(personas);
+
+	var count = req.query.count || 5;
+	var page = req.query.page || 1;
+
+	var filter = {
+		filters: {
+			mandatory: {
+				contains: req.query.filter
+			}
 		}
-	});
+	};
+
+	var pagination = {
+		start: (page - 1) * count,
+		count: count
+	};
+
+	var sort = {
+		sort: {
+			desc: 'apellido'
+		}
+	};
+
+	Persona.find().filter(filter).order(sort).page(pagination, function(err, personas) {
+ 		if (err) {
+ 			return res.status(400).send({
+ 				message: errorHandler.getErrorMessage(err)
+ 			});
+ 		} else {
+ 			res.jsonp(personas);
+ 		}
+ 	});
+
 };
 
 /**
