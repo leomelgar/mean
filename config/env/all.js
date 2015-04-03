9a4d62f801c64b58ec9cb57851fe7d1fd1feb842
leonux <leo.melgar33@gmail.com>
2015-04-03 11:55:54 -0300

    estilos dashboard.css para menu lateral izquierdo

--- config/env/all.js
+++ config/env/all.js
@@ -15,6 +15,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/bootstrap/dist/css/dashboard.css',
 				'public/lib/ng-table/dist/ng-table.css'
 			],
 			js: [
